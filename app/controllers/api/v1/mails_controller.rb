c08e265350325b769ed0f43a914a7e3e1c9c1ed2
SAKaNa567 <s1240147@u-aizu.ac.jp>
2020-02-15 16:10:15 +0900

    wip

--- app/controllers/api/v1/mails_controller.rb
+++ app/controllers/api/v1/mails_controller.rb
@@ -16,9 +16,6 @@ class Api::V1::MailsController < ApplicationController
         imap.fetch(ids, "RFC822").each do |mail|
             m = Mail.new(mail.attr["RFC822"])
             subject = m.subject
-            if  @mail.size >= 20
-                break
-            end
             if  subject.index("SEL,") == nil
                 next
             end
